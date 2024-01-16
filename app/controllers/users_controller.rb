6871b50d7be56545e4aa288768f14f9b30fc1214
Alexander Yakimec <admin@itpro.spb.ru>
2024-01-16 17:27:01 +0300

    Добавлена обработка turbo_stream для destroy и update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,23 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user!, only: %i[show edit update]
+  before_action :set_user!, only: %i[show edit update destroy]
 
   def index
     @users = User.order created_at: :desc
   end
 
+  def show
+  end
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
-    if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
-    else
-      render :edit
-    end
+    @user.update(user_params)
   end
 
-  def show
-    @user = User.find(params[:id])
+  def destroy
+    @user.destroy
+
   end
 
   private
