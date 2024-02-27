c4daccc5347fc2328e493df30cc737d3c5fcfa36
Alexander Yakimec <admin@itpro.spb.ru>
2024-02-27 18:01:05 +0300

    работа с паролем в формах

--- app/frontend/javascript/controllers/secure_records_controller.js
+++ app/frontend/javascript/controllers/secure_records_controller.js
@@ -2,7 +2,6 @@ import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
     copyToClipboard(text) {
-        // Пытаемся скопировать текст в буфер обмена
         navigator.clipboard.writeText(text)
             .catch(err => {
                 console.error('Ошибка копирования в буфер обмена: ', err);
@@ -24,12 +23,13 @@ export default class extends Controller {
 
     showPassword(e) {
         const button = e.currentTarget;
-        const password = document.getElementById('secure-record-password');
+        const password = document.querySelector('.secure-record-password');
+        let eyeIcon = button.closest('.show-buttons').querySelector('.eye-icon');
 
         if (button.classList.contains('closed')) {
             button.classList.replace('closed', 'opened')
             password.innerHTML = password.dataset.password;
-            document.getElementById('eye-icon').src = "/images/eye_opened.png";
+            eyeIcon.src = "/images/eye_opened.png";
             this.scaleButtonAnimation(button);
             return
         }
@@ -37,15 +37,46 @@ export default class extends Controller {
         if (button.classList.contains('opened')) {
             button.classList.replace('opened', 'closed')
             password.innerHTML = '********';
-            document.getElementById('eye-icon').src = "/images/eye_closed.png";
+            eyeIcon.src = "/images/eye_closed.png";
+            this.scaleButtonAnimation(button);
+        }
+    }
+
+    showPasswordInField(e) {
+
+        const button = e.currentTarget;
+        const password = button.closest('.form-field').querySelector('.field-secure-record-password');
+        let eyeIcon = button.closest('.show-buttons').querySelector('.eye-icon');
+
+        if (button.classList.contains('closed')) {
+            button.classList.replace('closed', 'opened')
+            password.type = 'text';
+            eyeIcon.src = "/images/eye_opened.png";
+            this.scaleButtonAnimation(button);
+            return
+        }
+
+        if (button.classList.contains('opened')) {
+            button.classList.replace('opened', 'closed');
+            password.type = 'password';
+            eyeIcon.src = "/images/eye_closed.png";
             this.scaleButtonAnimation(button);
         }
     }
 
     copyPassword(e) {
         const button = e.currentTarget;
-        const password = document.getElementById('secure-record-password');
-        this.copyToClipboard(password.dataset.password);
+        const password = document.querySelector('.secure-record-password');
+        if ('password' in password.dataset && password.dataset.password.length > 0) {
+            this.copyToClipboard(password.dataset.password);
+        }
+        this.scaleButtonAnimation(button);
+    }
+
+    copyPasswordInField(e) {
+        const button = e.currentTarget;
+        const password = button.closest('.form-field').querySelector('.field-secure-record-password');
+        this.copyToClipboard(password.value);
         this.scaleButtonAnimation(button);
     }
 
@@ -54,5 +85,89 @@ export default class extends Controller {
         const login = document.getElementById('secure-record-login');
         this.copyToClipboard(login.innerText);
         this.scaleButtonAnimation(button);
+    }
+
+    generatePassword(e) {
+        const button = e.currentTarget;
+        const password = button.closest('.form-field').querySelector('.field-secure-record-password');
+        this.scaleButtonAnimation(button);
+
+        function randomPassword(length) {
+            const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%&()-_=+";
+            let randomPassword = "";
+            for (let i = 0; i < length; ++i) {
+                const randomIndex = Math.floor(Math.random() * charset.length);
+                randomPassword += charset[randomIndex];
+            }
+            return randomPassword;
+        }
+
+        password.value = randomPassword(12);
+        this.checkSecurityOfPassword();
+    }
+
+    checkSecurityOfPassword(e) {
+
+        console.log('checking...')
+
+        let passwordField = document.querySelector('.field-secure-record-password');
+        let password = passwordField.value;
+
+        let passwordFieldDefaultBorderColor = 'rgba(195, 231, 255, 0.27)';
+
+        if (password.length > 3) {
+            passwordField.style.borderColor = 'red';
+            return
+        } else if (password.length < 3) {
+            passwordField.style.borderColor = passwordFieldDefaultBorderColor;
+            return
+        }
+
+
+        // let strength = calculatePasswordStrength(password);
+        //
+        // if (strength >= 60) {
+        //     passwordField.style.borderColor = '$cyber_red';
+        // } else if (strength >= 30) {
+        //     passwordField.style.borderColor = '$cyber_yellow';
+        // } else {
+        //     passwordField.style.borderColor = '$cyber_green';
+        // }
+        //
+        // function calculatePasswordStrength(password) {
+        //  let length = password.length;
+        //  let hasUpperCase = /[A-Z]/.test(password);
+        //  let hasLowerCase = /[a-z]/.test(password);
+        //  let hasNumbers = /[0-9]/.test(password);
+        //  let hasSpecialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
+        //
+        //  let strength = 0;
+        //
+        //     // Проверка длины пароля
+        //     if (length >= 8) {
+        //         strength += 10;
+        //     } else if (length >= 12) {
+        //         strength += 20;
+        //     }
+        //
+        //     // Проверка наличия букв в разных регистрах
+        //     if (hasUpperCase && hasLowerCase) {
+        //         strength += 20;
+        //     }
+        //
+        //     // Проверка наличия цифр
+        //     if (hasNumbers) {
+        //         strength += 15;
+        //     }
+        //
+        //     // Проверка наличия специальных символов
+        //     if (hasSpecialChars) {
+        //         strength += 15;
+        //     }
+        //
+        //     // Дополнительные проверки:
+        //     // Вы можете добавить свои собственные критерии оценки безопасности пароля здесь, например, проверку на наличие последовательных символов (12345, qwerty и т. д.).
+        //
+        //     return strength;
     }
 }
