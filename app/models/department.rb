db9c9ec083fc451000a11ed8ae21479f9ad4f643
Alexander Yakimec <admin@itpro.spb.ru>
2024-03-07 09:58:44 +0300

    some work

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,4 +2,12 @@ class Department < ApplicationRecord
   validates :key, :name, presence: true, uniqueness: true
   audited
   has_many :users
+
+  def show_name
+    if self.name.present?
+      self.name
+    else
+      self.key
+    end
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,12 @@ class Role < ApplicationRecord
   validates :key, presence: true, uniqueness: true
   audited
   has_many :users
+
+  def show_name
+    if self.name.present?
+      self.name
+    else
+      self.key
+    end
+  end
 end
