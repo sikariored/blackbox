7a2dc71b101024a28b46a7c9658fbd2ec6a31c73
Alexander Yakimec <admin@itpro.spb.ru>
2024-03-12 12:55:14 +0300

    Локализовал на русский язык письмо для сброса пароля

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,17 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  Rails.application.routes.default_url_options[:host] = '192.168.7.123:3000'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: 'blackgn13@gmail.com',
+    password: 'sfso qfvd wspy jqov',
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 end
